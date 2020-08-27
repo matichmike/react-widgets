6fd17a2d69e026ca7de028c37b3c0ca81b3a09c0
Michael Matich <matich.mike@gmail.com>
2020-08-27 16:26:48 -0400

    initial useffect set

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,17 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 const Search = () => {
   const [term, setTerm] = useState('');
+
+  useEffect(() => {
+   const search = async () => {
+    await axios.get('whatevr');
+   };
+
+   search();
+  }, [term])
+
   return (
     <div>
       <div className="ui form">
