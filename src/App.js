bf5e41fe775e5b2605154c5b8fed993f9d654c9c
Michael Matich <matich.mike@gmail.com>
2020-08-28 09:05:01 -0400

    color selector initial component added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import DropDown from './components/DropDown'
 
 const items = [
   {
@@ -17,10 +18,25 @@ const items = [
   }
 ]
 
+const options = [
+  {
+    label: 'The color red',
+    value: 'red'
+  },
+  {
+    label: 'The color green',
+    value: 'green'
+  },
+  {
+    label: 'The color blue',
+    value: 'blue'
+  }
+];
+
 export default () => {
   return (
     <div>
-      <Search />
+      <DropDown options={options} />
     </div>
   )
 };
